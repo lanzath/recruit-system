9902d27146f76a82abd2ca6592d6778d6bda49d2
Thiago Lanza <lanzathm@gmail.com>
2021-03-05 19:18:22 -0300

    Form request validation for Candidate

---
+++ backend/app/Http/Requests/Candidate/UpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Candidate;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|string|email|min:3|max:255',
+            'age' => 'required|integer',
+            'linkedin_url' => 'required|string|min:3|max:255',
+        ];
+    }
+}
