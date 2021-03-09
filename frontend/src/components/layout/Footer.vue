6fbfdc1474919fa3d8d3507a2d361312a180af0e
Thiago Lanza <lanzathm@gmail.com>
2021-03-09 09:00:19 -0300

    Header and footer components

---
+++ frontend/src/components/layout/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <p class="copy">Feito com <span>&hearts;</span> por Thiago Lanza</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+ .container {
+   position: fixed;
+   bottom: 0;
+   height: 50px;
+   width: 100vw;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   background-color: #1485ee;
+ }
+ p.copy {
+   font-size: 14px;
+   color: white;
+   font-weight: bold;
+ }
+</style>
---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header-container">
+    <ul class="menu">
+      <li><router-link to="/login">Home</router-link></li>
+      <li><router-link to="/">Candidatos</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+ .header-container {
+   height: 60px;
+   width: 100%;
+   display: flex;
+   background-color: #1485ee;
+ }
+
+ .header-container ul.menu {
+   display: flex;
+   width: 100vw;
+   gap: 20px;
+   align-items: center;
+   justify-content: center;
+   list-style: none;
+ }
+
+ .header-container ul.menu li a {
+   text-decoration: none;
+   font-size: 20px;
+   color: white;
+ }
+</style>
