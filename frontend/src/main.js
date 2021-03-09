e5acf19a00a423bcebef706bcda0d8cfaed75f9c
Thiago Lanza <lanzathm@gmail.com>
2021-03-08 21:47:47 -0300

    Axios and bootstrap add

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import Axios from 'axios'
+import { BootstrapVue } from 'bootstrap-vue'
 
 import './styles/global.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios
+
+Vue.use(BootstrapVue)
+
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   render: h => h(App),
