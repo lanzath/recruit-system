37cdb2173fdf0eb608cd503338d5238b9221d2b4
Thiago Lanza <lanzathm@gmail.com>
2021-03-08 21:47:12 -0300

    New candidate form

---
+++ frontend/src/components/Form.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="container">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group-1" label="Nome do Candidato:" label-for="input-1">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder="Nome"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-2"
+        label="Email:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+
+        id="input-group-3"
+        label="linkedin:"
+        label-for="input-3"
+      >
+        <b-form-input
+          id="input-3"
+          v-model="form.linkedin_url"
+          type="url"
+          placeholder="linkedin"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Idade:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          type="number"
+          v-model="form.age"
+          placeholder="Idade"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-5">
+        <b-form-checkbox-group
+          v-model="form.technology"
+          id="checkboxes-5"
+        >
+          <b-form-checkbox value="C#">C#</b-form-checkbox>
+          <b-form-checkbox value="Javascript">Javascript</b-form-checkbox>
+          <b-form-checkbox value="NodeJS">NodeJS</b-form-checkbox>
+          <b-form-checkbox value="Angular">Angular</b-form-checkbox>
+          <b-form-checkbox value="React">React</b-form-checkbox>
+          <b-form-checkbox value="Ionic">Ionic</b-form-checkbox>
+          <b-form-checkbox value="Mensageria">Mensageria</b-form-checkbox>
+          <b-form-checkbox value="PHP">PHP</b-form-checkbox>
+          <b-form-checkbox value="Laravel">Laravel</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <!-- <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card> -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Form',
+  props: {},
+
+  data() {
+      return {
+        form: {
+          name: '',
+          email: '',
+          linkedin_url: '',
+          age: null,
+          technology: []
+        },
+        show: true
+      }
+    },
+
+    methods: {
+      async onSubmit(event) {
+        event.preventDefault()
+        this.handleTechnologies()
+        const response = await axios.post('http://127.0.0.1:8000/api/v1/candidates', this.form)
+        this.$emit('res', response)
+        this.onReset(event)
+      },
+
+      handleTechnologies() {
+        const techs = this.form.technology
+        this.form.technology = []
+
+        techs.forEach(t => this.form.technology.push({ technology: t}))
+      },
+
+      onReset(event) {
+        event.preventDefault()
+
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.age = ''
+        this.form.linkedin_url = ''
+        this.form.technology = []
+
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
