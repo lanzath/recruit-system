770295622b01e25892ce4bc4061d31d4ac29b704
Thiago Lanza <lanzathm@gmail.com>
2021-03-14 23:24:23 -0300

    BelongsTo Relationship added

--- backend/app/Models/CandidateTechnology.php
+++ backend/app/Models/CandidateTechnology.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CandidateTechnology extends Model
 {
@@ -28,4 +29,18 @@ class CandidateTechnology extends Model
         'created_at' => 'datetime:Y-m-d h:m',
         'updated_at' => 'datetime:Y-m-d h:m',
     ];
+
+    //-------------------------------------------
+    // Relations
+    //-------------------------------------------
+
+    /**
+     * Candidate relationship
+     *
+     * @return BelongsTo
+     */
+    public function candidate(): BelongsTo
+    {
+        return $this->belongsTo(Candidate::class);
+    }
 }
